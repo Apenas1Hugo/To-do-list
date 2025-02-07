0fb8ccbdf44a86d662cd81d135e8c7c4aff01783
Apenas1Hugo <hugorodriguesm27@gmail.com>
2025-02-07 17:06:37 -0300

    JavaScript
    comecei a mexer no JavaScript

---
+++ script.js
@@ -0,0 +1,9 @@
+const inserir = document.querySelector("#digitar-tarefa");
+const btninserir = document.querySelector("#btninserir");
+
+const adicionartarefa = () => {
+    
+
+};
+
+btninserir.addEventListener("click", adicionartarefa );
