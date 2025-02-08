1ebc7d34ab1b03dad01853b6132c8861950df903
Apenas1Hugo <hugorodriguesm27@gmail.com>
2025-02-07 23:09:56 -0300

    adição das tarefas
    grande parte do JavaScript está feita, já é possível adicionar tarefas e marcar que elas foram concluídas falta apenas arrumar o botão para remover as tarefas.

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
 const inserir = document.querySelector("#digitar-tarefa");
 const btninserir = document.querySelector("#btninserir");
+const listatarefas = document.querySelector("#lista-tarefas")
 
 const adicionartarefa = () => {
-    
+    if (inserir.value.trim() === "") return;
+    listatarefas.style.background = "white"
 
+    const item = document.createElement("li");
+    const verificar = document.createElement("input");
+    const conteudo = document.createElement("span");
+    const botaoremove = document.createElement("button");
+
+    verificar.type = "checkbox" ;
+    conteudo.textContent = inserir.value;
+    botaoremove.textContent = "🗑";
+    botaoremove.style.marginLeft = "10px";
+
+    item.appendChild(verificar);
+    item.appendChild(conteudo);
+    item.appendChild(botaoremove);
+
+    listatarefas.appendChild(item);
+
+    inserir.value = "";
 };
 
 btninserir.addEventListener("click", adicionartarefa );
